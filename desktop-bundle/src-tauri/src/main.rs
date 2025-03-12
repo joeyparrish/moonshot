0cbbb35f7c9ad0a6c2c8f373f53c27c7e5e335b6
Joey Parrish <joey.parrish@gmail.com>
2025-03-11 20:36:02 -0700

    Add commented-out debugging stanza to Tauri setup

--- desktop-bundle/src-tauri/src/main.rs
+++ desktop-bundle/src-tauri/src/main.rs
@@ -1,8 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
   tauri::Builder::default()
+    /* Uncomment for debugging
+    .setup(|app| {
+      {
+        let window = app.get_webview_window("main").unwrap();
+        window.open_devtools();
+      }
+      Ok(())
+    })
+    */
     .run(tauri::generate_context!("tauri.conf.json"))
     .expect("error while running tauri application");
 }
