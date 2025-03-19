6bcb77d9bcf83352c89c9dc4dc747cd4380728df
Joey Parrish <joey.parrish@gmail.com>
2025-03-19 14:34:13 -0700

    Add support for scenes to stop the music

--- web-interface/music.js
+++ web-interface/music.js
@@ -1,6 +1,6 @@
 async function startMusic() {
-  // FIXME: support disabling music
   if (!backgroundMusic.paused && !backgroundMusic.ended) return;
+  if (!backgroundMusic.src) return;
 
   try {
     await backgroundMusic.play();
@@ -10,11 +10,30 @@ async function startMusic() {
 let backgroundMusicLoopTo = 0;
 
 function setBackgroundMusic(url, loopTo, credit, link) {
-  backgroundMusicLoopTo = loopTo;
+  // The audio element has a canonical URL, so we have to canonicalize the
+  // input to compare.
+  url = (new URL(url, location.href)).href;
+  if (backgroundMusic.src == url) {
+    // Already playing this one.
+    return;
+  }
+
   backgroundMusic.src = url;
+  backgroundMusicLoopTo = loopTo;
   backgroundMusicCredit.textContent = credit;
   backgroundMusicLink.href = link;
+  backgroundMusicLink.style.display = 'inline';
+  noMusic.style.display = 'none';
+
   startMusic();
+}
+
+function stopBackgroundMusic() {
+  backgroundMusic.pause();
+  backgroundMusic.src = '';
+
+  noMusic.style.display = 'inline';
+  backgroundMusicLink.style.display = 'none';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
