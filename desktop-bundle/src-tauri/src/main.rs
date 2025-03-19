b8e937428a5e04da0198ca13fe3ea8aa7d7468f2
Joey Parrish <joey.parrish@gmail.com>
2025-03-19 13:16:48 -0700

    Fix tauri to open links in default browser

--- desktop-bundle/src-tauri/src/main.rs
+++ desktop-bundle/src-tauri/src/main.rs
@@ -4,16 +4,17 @@
 use tauri::Manager;
 
 fn main() {
-  tauri::Builder::default()
-    /* Uncomment for debugging
-    .setup(|app| {
-      {
-        let window = app.get_webview_window("main").unwrap();
-        window.open_devtools();
-      }
-      Ok(())
-    })
-    // */
-    .run(tauri::generate_context!("tauri.conf.json"))
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        /* Uncomment for debugging
+        .setup(|app| {
+            {
+                let window = app.get_webview_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
+        // */
+        .run(tauri::generate_context!("tauri.conf.json"))
+        .expect("error while running tauri application");
 }
