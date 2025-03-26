1640884286bfaf9e1559d04813f8c8610452870f
Joey Parrish <joey.parrish@gmail.com>
2025-03-26 13:26:32 -0700

    Fix keyboard scrolling during pause

--- web-interface/pause.js
+++ web-interface/pause.js
@@ -11,6 +11,11 @@ function unpauseGame() {
   document.querySelector('.continue')?.remove();
 }
 
+const UNPAUSE_KEYS = [
+  'Enter',
+  'Escape',
+];
+
 function pauseGame() {
   // Hide the prompt.
   vorple.prompt.hide();
@@ -20,22 +25,38 @@ function pauseGame() {
   // Wait for a click or keypress, just once.
   const vorpleElement = document.getElementById('vorple');
   const listener = (event) => {
-    if (event.type == 'keydown' &&
-        event.key != 'Enter' && event.key != 'Escape') {
-      // Ignore keys other than escape or enter.  Especially, say Alt+Tab.  So
-      // annoyed by having the pause dismissed by modifier keys.
-      return;
+    const endPauseState = () => {
+      // Defer unpausing, to make sure this event is dead before the
+      // interpreter moves again.  This allows us to pause immediately after
+      // unpausing if we have a series of screens we want to pause on.
+      setTimeout(() => unpauseGame(), 100);
+
+      // Either event is enough to cancel listening for the other.
+      // Because of this, we don't use "once" in the listener setup.
+      vorpleElement.removeEventListener('click', listener, {capture: true});
+      document.removeEventListener('keydown', listener, {capture: true});
+    };
+
+    if (event.type == 'keydown') {
+      // Key events have a few different possible paths.
+      if (UNPAUSE_KEYS.includes(event.key)) {
+        endPauseState();
+      } else if (event.key == 'PageUp' || event.key == 'PageDown') {
+        // Navite keyboard scrolling doesn't work for some unknown reason when
+        // we're paused, so emulate it here, since we're already handling key
+        // events in this state.
+        const signY = event.key == 'PageDown' ? 1 : -1;
+        const deltaY = vorpleElement.clientHeight * signY * 0.8;
+        vorpleElement.scrollBy(0, deltaY);
+      } else {
+        // Ignore other keys.  Especially, say, Alt+Tab.
+        // So annoyed by having the pause dismissed by modifier keys.
+        return;
+      }
+    } else {
+      // Click event.  Unconditionally end the pause state.
+      endPauseState();
     }
-
-    // Defer unpausing, to make sure this event is dead before the interpreter
-    // moves again.  This allows us to pause immediately after unpausing if we
-    // have a series of screens we want to pause on.
-    setTimeout(() => unpauseGame(), 100);
-
-    // Either event is enough to cancel listening for the other.
-    // Because of this, we don't use "once" in the listener setup.
-    vorpleElement.removeEventListener('click', listener, {capture: true});
-    document.removeEventListener('keydown', listener, {capture: true});
   };
   vorpleElement.addEventListener('click', listener, {capture: true});
   document.addEventListener('keydown', listener, {capture: true});
